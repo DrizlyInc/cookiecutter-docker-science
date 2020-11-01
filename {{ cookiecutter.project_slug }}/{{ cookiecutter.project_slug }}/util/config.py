8ce9acf5fe327c3b757b25f6ae7df44442b78e96
jakeatdrizly <jsmart@drizly.com>
2020-10-31 21:26:32 -0400

    Adding environment variable arg to create-container

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/util/config.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/util/config.py
@@ -4,6 +4,6 @@ import os
 #TODO: Figure out how to pass envirorment variables and possibly still support sagemaker SLA
 env = os.environ.get('ENV')
 # dev, local, or prod
-with open(f"../../config/{env}_resource.yaml", "r") as ymlfile:
+with open(f"../../environments/config/{env}_resource.yaml", "r") as ymlfile:
   cfg = Box(yaml.safe_load(ymlfile))
 
