7b2247f7ea2a0a482ff15022494a509ee79bcfa0
jakeatdrizly <jsmart@drizly.com>
2020-11-13 18:23:22 -0500

    [DS-612] Making the template flow a little bit more of an action just to give a clearer sense of how to get pandas dataframes and convert types for different solids

--- {{ cookiecutter.project_slug }}/dagster/tests/test_main.py
+++ {{ cookiecutter.project_slug }}/dagster/tests/test_main.py
@@ -2,28 +2,59 @@
 import pytest
 from dagster import execute_pipeline, execute_solid
 from repos.{{cookiecutter.dagster_repo}}.{{cookiecutter.project_slug}}.dagster.main import (
-    get_{{cookiecutter.project_slug}}_list,
+    get_{{cookiecutter.project_slug}}_df,
+    transform_{{cookiecutter.project_slug}}_df,
+    {{cookiecutter.project_slug}}_df_to_list,
     write_{{cookiecutter.project_slug}}_to_redis,
 )
+
 from repos.{{cookiecutter.dagster_repo}}.{{cookiecutter.project_slug}}.dagster.modes import (
     dev_mode,
     local_mode,
     test_mode,
 )
+from repos.{{cookiecutter.dagster_repo}}.{{cookiecutter.project_slug}}.dagster.custom_types import (
+    {{cookiecutter.project_slug}}TransformedDF,
+    {{cookiecutter.project_slug}}DF,
+)
+
 from repos.{{cookiecutter.dagster_repo}}.{{cookiecutter.project_slug}}.ds_util.config import test_cfg
 
+import pandas as pd
 
 class Test{{cookiecutter.project_slug}}(unittest.TestCase):
     """Test {{cookiecutter.project_slug}}"""
 
     @pytest.mark.{{cookiecutter.dagster_repo}}
     def test_{{cookiecutter.project_slug}}_df(self):
-        """Store order items"""
+        """{{cookiecutter.project_slug}}"""
         result = execute_solid(
             get_{{cookiecutter.project_slug}}_list,
             mode_def=test_mode,
             run_config=test_cfg.to_dict(),
             input_values={"query": "SELECT 1"},
+        )
+        self.assertTrue(result)
+
+    @pytest.mark.{{cookiecutter.dagster_repo}}
+    def test_transform_{{cookiecutter.project_slug}}_df(context, df: {{cookiecutter.project_slug}}DF) -> {{cookiecutter.project_slug}}TransformedDF:
+        """Transform Snowflake Dataframe"""
+        result = execute_solid(
+            transform_{{cookiecutter.project_slug}}_df,
+            mode_def = test_mode,
+            run_config = test_cfg.to_dict(),
+            input_values = {"df": pd.read_csv("test.csv")},
+        )
+        self.assertTrue(result)
+
+    @pytest.mark.{{cookiecutter.dagster_repo}}
+    def {{cookiecutter.project_slug}}_df_to_list(context, df: {{cookiecutter.project_slug}}TransformedDF) -> List:
+        """Load Snowflake Data as List and log some information"""
+        result = execute_solid(
+            {{cookiecutter.project_slug}}_df_to_list,
+            mode_def = test_mode,
+            run_config = test_cfg.to_dict(),
+            input_values = {"df": pd.DataFrame(['1', '2'])},
         )
         self.assertTrue(result)
 
