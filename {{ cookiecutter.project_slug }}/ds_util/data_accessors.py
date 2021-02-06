b90e6e7243d2fe7df9b46fe0e8486ff7527e7ea6
jakeatdrizly <jsmart@drizly.com>
2021-02-05 21:33:27 -0500

    Updating to keep up with new dagster utils

--- {{ cookiecutter.project_slug }}/ds_util/data_accessors.py
+++ {{ cookiecutter.project_slug }}/ds_util/data_accessors.py
@@ -15,23 +15,6 @@ def snowflake_dataframe_from_sql(sql):
     result = snowflake_execute_query(sql)
     df = pd.DataFrame(result)
     df.columns = result.keys()
-    return dfimport pandas as pd
-from drizly_dagster_utils.integrations.mysql import MySQLConn
-from drizly_dagster_utils.integrations.snowflake import SnowflakeConn
-from ds_util.config import cfg
-
-
-def snowflake_execute_query(sql):
-    secret_name = cfg.resources.snowflake.config.secret_name
-    region = cfg.resources.snowflake.config.region
-    connection = SnowflakeConn.from_secret(f"{secret_name}", f"{region}")
-    return connection.execute_query_no_fetch(sql)
-
-
-def snowflake_dataframe_from_sql(sql):
-    result = snowflake_execute_query(sql)
-    df = pd.DataFrame(result)
-    df.columns = result.keys()
     return df
 
 
