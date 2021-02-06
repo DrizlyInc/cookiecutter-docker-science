944579fd9fcfad500ae31090f5c5ad7637d1dc44
jakeatdrizly <jsmart@drizly.com>
2021-02-05 21:32:02 -0500

    Updating to keep up with new dagster utils

--- {{ cookiecutter.project_slug }}/ds_util/data_accessors.py
+++ {{ cookiecutter.project_slug }}/ds_util/data_accessors.py
@@ -8,7 +8,24 @@ def snowflake_execute_query(sql):
     secret_name = cfg.resources.snowflake.config.secret_name
     region = cfg.resources.snowflake.config.region
     connection = SnowflakeConn.from_secret(f"{secret_name}", f"{region}")
-    return connection.execute_query(sql)
+    return connection.execute_query_no_fetch(sql)
+
+
+def snowflake_dataframe_from_sql(sql):
+    result = snowflake_execute_query(sql)
+    df = pd.DataFrame(result)
+    df.columns = result.keys()
+    return dfimport pandas as pd
+from drizly_dagster_utils.integrations.mysql import MySQLConn
+from drizly_dagster_utils.integrations.snowflake import SnowflakeConn
+from ds_util.config import cfg
+
+
+def snowflake_execute_query(sql):
+    secret_name = cfg.resources.snowflake.config.secret_name
+    region = cfg.resources.snowflake.config.region
+    connection = SnowflakeConn.from_secret(f"{secret_name}", f"{region}")
+    return connection.execute_query_no_fetch(sql)
 
 
 def snowflake_dataframe_from_sql(sql):
@@ -16,6 +33,21 @@ def snowflake_dataframe_from_sql(sql):
     df = pd.DataFrame(result)
     df.columns = result.keys()
     return df
+
+
+def mysql_execute_query(sql):
+    secret_name = cfg.resources.mysql.config.secret_name
+    region = cfg.resources.mysql.config.region
+    connection = MySQLConn.from_secret(f"{secret_name}", f"{region}")
+    return connection.execute_query(sql)
+
+
+def mysql_dataframe_from_sql(sql):
+    result = mysql_execute_query(sql)
+    df = pd.DataFrame(result)
+    df.columns = result.keys()
+    return df
+
 
 
 def mysql_execute_query(sql):
